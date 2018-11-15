97878aed7a5ac6b04abe368eb5251a527f838b45
Jose Ramón Trillo Vílchez <j.ra_foam@hotmail.com>
2018-11-15 17:25:59 +0100

    Arreglar el #10

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ app.get('/', function (req, res) {
 app.put('/:persona1/:persona2',function(req, res){
 	res.send(req.params.persona1+" y "+req.params.persona2+" se van de vacaciones juntos");
 });
+
+
+//se ha realizado esta modificación  en el listen en el ejercicio 4 debido a que, si no se realiza esto
+//el test en travis no para y se queda continuamente ejecutando
 var port=process.env.PORT || 3000
 if(!module.parent){
   app.listen(port);
-  console.log('Server running at http://127.0.0.1:' + port + '/');
+  console.log('El servicio está corriendo en el puerto si no para la has liado:' + port );
 }
 
 module.exports = app
