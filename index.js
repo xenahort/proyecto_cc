d2f33ef462af35f34bddfb2432b1e643daecf39f
Jose Ramón Trillo Vílchez <j.ra_foam@hotmail.com>
2018-11-16 22:21:31 +0100

    arreglar travis #11

--- index.js
+++ index.js
@@ -14,32 +14,37 @@ app.get('/', function (req, res) {
 
 //Leer los datos 
 app.get('/datos', function (req, res) {
+
   res.send(JSON.stringify(historial));
 });
 
 //crear
 app.post('/datos/:idtwitter/:iduser/:etiqueta/:dia/:mes/:anho/:hora/:minutos/:nlike/:nretweet', function(req,res){
+
   anhade(req.params.idtwitter,req.params.iduser,req.params.etiqueta,req.params.dia,req.params.mes,req.params.anho,req.params.hora,req.params.minutos,req.params.nlike,req.params,nretweet);
+
   res.send("comentario creado: "+JSON.stringify(historial[size+1]));
 
 });
-
 //editar
 app.put('/datos/:i/:idtwitter/:iduser/:etiqueta/:dia/:mes/:anho/:hora/:minutos/:nlike/:nretweet',function(req, res){
+
   editar(req.params.i,req.params.idtwitter,req.params.iduser,req.params.etiqueta,req.params.dia,req.params.mes,req.params.anho,req.params.hora,req.params.minutos,req.params.nlike,req.params,nretweet);
+
+
   res.send("comentario modificado "+JSON.stringify(historial[req.params.i]));
 
 });
-
 //borrar
 app.delete('/datos/:i',function(req,res){
+
   salida=borrardatos(req.params.i)
   res.send("comentario borrado: "+JSON.stringify(salida));
 });
 
 //se ha realizado esta modificación  en el listen en el ejercicio 4 debido a que, si no se realiza esto
 //el test en travis no para y se queda continuamente ejecutando
-var port= 3000
+var port=process.env.PORT || 3000
 if(!module.parent){
   app.listen(port);
   console.log('El servicio está corriendo en el puerto si no para la has liado: ' + port );
