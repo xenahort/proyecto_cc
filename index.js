8745f69d3ee7a0967b86b0616abb4cb60f36cd9e
Jose Ramón Trillo Vílchez <j.ra_foam@hotmail.com>
2019-01-17 16:51:23 +0100

    Anhadir MongoDB #18

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ var tamanho=0;
 //se crea la base de datos
 cliente.connect(url,{ useNewUrlParser: true }, function(err, db) {
   var inst = db.db("database");
-  inst.createCollection("aplicacion", function(err, res) {
+  inst.createCollection("aplicacion", function(err, res) { //https://docs.mongodb.com/manual/reference/method/db.createCollection/
     console.log("Se crea la base de datos");
     db.close();
   });
 });
 
+//creamos nuestro vector numérico llamada comentario.
 var comentario = {
   idtwitter:0,
   iduser:0,
@@ -25,6 +26,23 @@ var comentario = {
   nlikes:0,
   nretweet:0
 }
+
+//creamos la funcion añadir
+
+function anhade(idtwitter,iduser,etiqueta,dia,mes,anho,hora,min,nlikes,nretweet){
+  comentario.idtwitter = idtwitter;
+  comentario.iduser = iduser;
+  comentario.etiqueta = etiqueta;
+  comentario.dia = dia;
+  comentario.mes = mes;
+  comentario.anho = anho;
+  comentario.hora = hora;
+  comentario.min = min;
+  comentario.nlikes = nlikes;
+  comentario.nretweet = nretweet;
+ 
+}
+
 
 //Crear el ok
 aplicacion.get('/', function (req, res) {
@@ -52,7 +70,7 @@ aplicacion.post('/datos/:idtwitter/:iduser/:etiqueta/:dia/:mes/:anho/:hora/:minu
   anhade(req.params.idtwitter,req.params.iduser,req.params.etiqueta,req.params.dia,req.params.mes,req.params.anho,req.params.hora,req.params.minutos,req.params.nlike,req.params.nretweet);
    cliente.connect(url, { useNewUrlParser: true },function(err, db) {
     var inst = db.db("database");
-    inst.collection("aplicacion").insertOne(comentario, function(err, res) {
+    inst.collection("aplicacion").insertOne(comentario, function(err, res) {   //https://docs.mongodb.com/manual/reference/method/db.collection.insertOne/
       db.close();
     });
   });
@@ -64,12 +82,11 @@ aplicacion.post('/datos/:idtwitter/:iduser/:etiqueta/:dia/:mes/:anho/:hora/:minu
 //editar
 aplicacion.put('/datos/:i/:idtwitter/:iduser/:etiqueta/:dia/:mes/:anho/:hora/:minutos/:nlike/:nretweet',function(req, res){
   anhade(req.params.idtwitter,req.params.iduser,req.params.etiqueta,req.params.dia,req.params.mes,req.params.anho,req.params.hora,req.params.minutos,req.params.nlike,req.params.nretweet);
-  
   cliente.connect(url, { useNewUrlParser: true },function(err, db) {
     var inst = db.db("database");
     var mq = { idtwitter: req.params.idtwitter, iduser: req.params.iduser, etiqueta: req.params.etiqueta, dia: req.params.dia, mes: req.params.mes, anho: req.params.anho, hora: req.params.hora, min: req.params.minutos, nlikes: req.params.nlike, nretweet: req.params.nretweet};
     var nvalor = { $set: comentario }; 
-    inst.collection("aplicacion").updateOne(mq, nvalor, function(err, res) {
+    inst.collection("aplicacion").updateOne(mq, nvalor, function(err, res) { //https://docs.mongodb.com/manual/reference/method/db.collection.updateOne/
       db.close();
     });
   });
@@ -77,17 +94,15 @@ aplicacion.put('/datos/:i/:idtwitter/:iduser/:etiqueta/:dia/:mes/:anho/:hora/:mi
 
 });
 
-
 //borrar
 aplicacion.delete('/datos/:i',function(req,res){
   cliente.connect(url, { useNewUrlParser: true },function(err, db) {
     var inst = db.db("database");
     var mq = { idtwitter: req.params.idtwitter, iduser: req.params.iduser, etiqueta: req.params.etiqueta, dia: req.params.dia, mes: req.params.mes, anho: req.params.anho, hora: req.params.hora, min: req.params.minutos, nlikes: req.params.nlike, nretweet: req.params.nretweet};
-    inst.collection("aplicacion").deleteOne(mq, function(err, obj) {
+    inst.collection("aplicacion").deleteOne(mq, function(err, res) {  //https://docs.mongodb.com/manual/reference/method/db.collection.deleteOne/
       res.sendStatus(200);
       db.close();
     });
-
   });
   tamanho=tamanho-1;
 });
